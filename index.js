0ad90c90c6b3e49b0cc2327d8413e94d28067c77
Danish Shafi <danish.shafi@live.co.uk>
2020-06-24 13:40:21 +0100

    User story added

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function addSpanClass(event) {
-  const iconText = event.target.querySelector('.map-icon-text');
-  iconText.classList.add('map-icon-text-active');
+  const iconText = event.target.querySelector('.map__icon--text');
+  iconText.classList.add('map__icon--text-active');
 }
 function removeSpanClass(event) {
-  const iconText = event.target.querySelector('.map-icon-text');
-  iconText.classList.remove('map-icon-text-active');
+  const iconText = event.target.querySelector('.map__icon--text');
+  iconText.classList.remove('map__icon--text-active');
 }
 
-const mapIcon = document.querySelectorAll('.map-icon');
+const mapIcon = document.querySelectorAll('.map__icon');
 for (i = 0; i < mapIcon.length; i++) {
   mapIcon[i].addEventListener('mouseenter', addSpanClass);
   mapIcon[i].addEventListener('mouseleave', removeSpanClass);
